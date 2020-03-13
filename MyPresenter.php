0eee85de0049b361d42c363cd06b002cb4cc16ad
IgorKonevsky <60656783+IgorKonevsky@users.noreply.github.com>
2020-03-13 22:46:43 +0300

    13.03

--- MyPresenter.php
+++ MyPresenter.php
@@ -1,12 +1,7 @@
 <?php
 
 class MyPresenter extends _MainPresenter{
-    public $text = array(
-        "one" => 1,
-        "two" => 2,
-        "three" => 3,
-        
-    );
+    public $text = ["one", "two", "three"];
     
      public function getText(){
         echo json_encode($this->text);
