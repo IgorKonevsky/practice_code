027c6a3dbb5b18068c23db80a62e87c38623614e
IgorKonevsky <60656783+IgorKonevsky@users.noreply.github.com>
2020-03-16 16:40:13 +0300

    update

---
+++ g2_public/app/models/Test.php
@@ -0,0 +1,13 @@
+<?php
+
+class Test extends _MainModel{
+	public $text = ["one", "two", "three"];
+    
+    public function getText(){
+        echo json_encode($this->text);
+    }
+
+}
+
+
+?>
--- g2_public/app/presenters/MyPresenter.php
+++ g2_public/app/presenters/MyPresenter.php
@@ -1,10 +1,10 @@
 <?php
 
 class MyPresenter extends _MainPresenter{
-    public $text = ["one", "two", "three"];
+    
     
      public function getText(){
-        echo json_encode($this->text);
+        echo (new Test())->getText();
     }
     
 }
